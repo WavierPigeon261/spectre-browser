0a87871e294799950596d23703b217bbebde957c
Sourabh Shet <160525160+WavierPigeon261@users.noreply.github.com>
2026-08-16 15:20:52 +0530

    Restore

--- src/Profiles/Default/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.taskbar.lists.enabled", false);
-user_pref("browser.shell.checkDefaultBrowser", false);
-user_pref("app.title", "Spectre Search");
