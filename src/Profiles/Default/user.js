b330dad5dd7cbce95b55f4ef3da1034ceb4cf96e
Sourabh Shet <160525160+WavierPigeon261@users.noreply.github.com>
2026-08-16 15:13:11 +0530

    Upload #1

---
+++ src/Profiles/Default/user.js
@@ -0,0 +1,4 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.taskbar.lists.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("app.title", "Spectre Search");
